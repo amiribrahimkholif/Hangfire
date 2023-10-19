085b57023f685a7a1bf21121d7e2b2c85c24d8b4
amiribrahimkholif <amiribrahimkholif@gmail.com>
2023-10-19 13:57:03 +0300

    Try all Hangfire methods as a practice

--- Hangfire/Controllers/WeatherForecastController.cs
+++ Hangfire/Controllers/WeatherForecastController.cs
@@ -18,9 +18,32 @@ namespace Hangfire.Controllers
             _logger = logger;
         }
 
-        [HttpGet(Name = "GetWeatherForecast")]
+        [HttpGet(Name ="Get")]
         public IEnumerable<WeatherForecast> Get()
         {
+            // Enqueue is Fire-and-forget method. 
+            // It make the background job scenario but it executed without delay. 
+            BackgroundJob.Enqueue(() => Greeting("Enqueue"));
+
+
+            // Schedule is Scheduled method run after a determine time interval. 
+            // It make the background job scenario but it executed with delay. 
+            BackgroundJob.Schedule(() => Greeting("Schedule"),TimeSpan.FromMinutes(1));
+
+
+            // The recurring job run every time unit you pass it. 
+            // Take a Cron expression that will work with respect to it.
+            RecurringJob.AddOrUpdate("GreetingEveryDay12AM",() => Greeting("Schedule"), "0  0 * * *");
+
+            RecurringJob.AddOrUpdate("GreetingEveryMinute",() => Greeting("Recurring"), Cron.Minutely);
+
+            RecurringJob.RemoveIfExists("GreetingEveryDay12AM");
+
+            RecurringJob.Trigger("GreetingEveryMinute");
+
+
+
+
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
@@ -29,5 +52,14 @@ namespace Hangfire.Controllers
             })
             .ToArray();
         }
+
+        [ApiExplorerSettings(IgnoreApi = true)]
+        public void Greeting(string where)
+        {
+            // Run and Forget => take the same process of serializing and deserializing and then run.
+           Console.WriteLine($"Hello form {where}.");
+        }
+
+
     }
 }
--- Hangfire/Program.cs
+++ Hangfire/Program.cs
@@ -1,6 +1,15 @@
+using Hangfire;
+using Microsoft.Extensions.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
+
 // Add services to the container.
+builder.Services.AddHangfire(x => x.UseSqlServerStorage(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddHangfireServer();
+
+// Add the processing server as IHostedService
+builder.Services.AddHangfireServer();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -21,5 +30,11 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.UseHangfireDashboard("/dashboard");
+
+IBackgroundJobClient backgroundJobs = new BackgroundJobClient();
+backgroundJobs.Enqueue(()=>Console.WriteLine("Hello from hangfire at Program.cs"));
+backgroundJobs.Enqueue(()=>Console.WriteLine("Hello from hangfire at Program.cs"));
 
 app.Run();
